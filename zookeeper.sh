97de67fc1a47ea34c66e34e12e5877af6969d383
T H Beh <thbeh@thbeh.com>
2019-04-26 17:28:54 +1200

    v1.5
    add drill installation

--- zookeeper.sh
+++ zookeeper.sh
@@ -7,7 +7,10 @@ apt-get install openjdk-8-jdk -y
 cd /usr/local
 
 wget https://www-eu.apache.org/dist/zookeeper/stable/zookeeper-3.4.14.tar.gz
+wget https://www-us.apache.org/dist/drill/drill-1.15.0/apache-drill-1.15.0.tar.gz
+
 tar -xvf "zookeeper-3.4.14.tar.gz"
+tar -zxvf "apache-drill-1.15.0.tar.gz"
 
 touch zookeeper-3.4.14/conf/zoo.cfg
 
@@ -29,3 +32,13 @@ mkdir -p /var/lib/zookeeper
 echo $(($1+1)) >> /var/lib/zookeeper/myid
 
 zookeeper-3.4.14/bin/zkServer.sh start
+
+mv apache-drill-1.15.0/conf/drill-override.conf apache-drill-1.15.0/conf/drill-override.bak
+
+cat < EOF > apache-drill-1.15.0/conf/drill-override.conf 
+drill.exec: {
+	cluster-id: "zdsdrillcluster"
+	zk.connect: "10.140.193.36:2181,10.140.193.37:2181,10.140.193.38:2181"
+}
+EOF
+
